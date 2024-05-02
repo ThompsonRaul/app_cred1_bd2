6c79b590de301fdd7cb6f6265e390d7484e70f98
ThompsonRaul <vtrs2001@hotmail.com>
2024-05-02 14:41:16 -0300

    Att final 3

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,46 @@
 import Link from "next/link";
+import React from "react";
 
 function Home() {
+  const links = [
+    { href: "/books", label: "Listar Livros" },
+    { href: "/books/details", label: "Listar Livro Específico" },
+    { href: "/reviews", label: "Listar Avaliações" },
+    { href: "/reviews/details", label: "Listar Avaliações por Usuário" },
+    { href: "/users", label: "Listar Usuários" },
+    { href: "/authors", label: "Listar Autores" },
+    { href: "/books/most_recent", label: "Mostrar Livro Mais Recente" },
+    { href: "/books/books_per_author", label: "Listar Livros por Autor" },
+    {
+      href: "/reviews/reviews_per_book",
+      label: "Listar Média de Avaliações por Livro",
+    },
+    { href: "/books/post", label: "Inserir Livro" },
+    { href: "/reviews/post", label: "Inserir Avaliação" },
+    { href: "/books/delete", label: "Deletar Livro" },
+    { href: "/books/delete/deleted_books", label: "Mostrar Livros Deletados" },
+    { href: "/authors/update", label: "Atualizar Autor" },
+    { href: "/books/update", label: "Atualizar Livro" },
+    { href: "/books/count", label: "Contagem de Livros" }, // Esta linha foi adicionada para incluir o último item.
+  ];
+
+  // Ordenando os links por rótulo para exibição organizada
+  links.sort((a, b) => a.label.localeCompare(b.label));
+
   return (
     <>
       <div>
         <h1>Segunda parte do crédito de BD II</h1>
         <h2>Escolha o que quer fazer:</h2>
-        <Link href="/books">Listar Livros</Link>
-        <br></br>
-        <Link href="/books/details">Listar Livro Específico</Link>
-        <br></br>
-        <Link href="/reviews">Listar Avaliações</Link>
-        <br></br>
-        <Link href="/reviews/details">Listar Avaliações por Usuário</Link>
-        <br></br>
-        <Link href="/users">Listar Usuários</Link>
-        <br></br>
-        <Link href="/authors">Listar Autores</Link>
-        <br></br>
-        <Link href="/books/most_recent">Mostrar Livro Mais Recente</Link>
-        <br></br>
-        <Link href="/books/books_per_author">Listar Livros por Autor</Link>
-        <br></br>
-        <Link href="/reviews/reviews_per_book">
-          Listar Média de Avaliações por Livro
-        </Link>
-        <br></br>
-        <Link href="/books/post">Inserir Livro</Link>
-        <br></br>
-        <Link href="/reviews/post">Inserir Avaliação</Link>
-        <br></br>
-        <Link href="/books/delete">Deletar Livro</Link>
-        <br></br>
-        <Link href="/books/delete/deleted_books">Mostrar Livros Deletados</Link>
-        <br></br>
-        <Link href="/authors/update">Atualizar Autor</Link>
-        <br></br>
-        <Link href="/books/update">Atualizar Livro</Link>
-        <br></br>
-        <Link href="/books/count">Contagem de Livros</Link>
+        {links.map((link, index) => (
+          <React.Fragment key={index}>
+            <Link href={link.href}>{link.label}</Link>
+            <br />
+          </React.Fragment>
+        ))}
       </div>
     </>
   );
 }
+
 export default Home;
